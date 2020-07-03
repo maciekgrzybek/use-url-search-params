cb832f6c9f129204c54b50dc34b561dd4914761d
Maciej Grzybek <maciekgrzybek1@gmail.com>
2020-07-03 16:00:20 +0100

    Add more hooks and test cases

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -2,6 +2,10 @@ import {
   useHasSearchParam,
   useGetSearchParam,
   useAppendSearchParam,
+  useSetSearchParam,
+  useDeleteSearchParam,
+  useGetAllSearchParams,
+  useStringifySearchParams,
 } from '../src';
 
 describe('useHasSearchParam', () => {
@@ -14,8 +18,14 @@ describe('useHasSearchParam', () => {
 });
 
 describe('useGetSearchParam', () => {
-  test('returns value if param exists', () => {
+  test('returns single value if param exists', () => {
     expect(useGetSearchParam('?topic=api', 'topic')).toBe('api');
+  });
+  test('returns an array of values associated with a given search param', () => {
+    expect(useGetSearchParam('?topic=api&topic=another-api', 'topic')).toEqual([
+      'api',
+      'another-api',
+    ]);
   });
   test('returns null if param does not exist', () => {
     expect(useGetSearchParam('?topic=api', 'not-topic')).toBeNull();
@@ -24,28 +34,134 @@ describe('useGetSearchParam', () => {
 
 describe('useAppendSearchParam', () => {
   test('appends single param to the url', () => {
-    expect(
-      useAppendSearchParam('?topic=api', { key: 'new-topic', value: 'new-api' })
-    ).toBe('topic=api&new-topic=new-api');
+    expect(useAppendSearchParam('?topic=api', { 'new-topic': 'new-api' })).toBe(
+      'topic=api&new-topic=new-api'
+    );
   });
   test('appends multiple params to the url', () => {
     expect(
-      useAppendSearchParam('?topic=api', [
-        { key: 'new-topic', value: 'new-api' },
-        { key: 'newer-topic', value: 'newer-api' },
-      ])
+      useAppendSearchParam('?topic=api', {
+        'new-topic': 'new-api',
+        'newer-topic': 'newer-api',
+      })
     ).toBe('topic=api&new-topic=new-api&newer-topic=newer-api');
   });
 });
 
-test('sets up new value to existing  param', () => {});
-test('deletes existing param', () => {});
-test('returns all params with their values', () => {});
-test('returns all keys of the params', () => {});
-test('returns all values of the params', () => {});
-test('returns all the values associated with a given search param', () => {});
-test('Sorts all key/value pairs by their keys.', () => {});
+describe('useSetSearchParam', () => {
+  test('appends single param in the url', () => {
+    expect(useSetSearchParam('?topic=api', { topic: 'new-api' })).toBe(
+      'topic=new-api'
+    );
+  });
+  test('appends multiple params in the url', () => {
+    expect(
+      useSetSearchParam('?topic=api&new-topic=api', {
+        topic: 'api-set',
+        'new-topic': 'new-api-set',
+      })
+    ).toBe('topic=api-set&new-topic=new-api-set');
+  });
+});
+describe('useDeleteSearchParam', () => {
+  describe('deletes single param from the url', () => {
+    test('when url has one param', () => {
+      expect(useDeleteSearchParam('?topic=api', 'topic')).toBe('');
+    });
+    test('when url has multiple params', () => {
+      expect(
+        useDeleteSearchParam('?topic=api&other-topic=other-api', 'topic')
+      ).toBe('other-topic=other-api');
+    });
+  });
+  describe('deletes multiple params from the url', () => {
+    test('when url has two params', () => {
+      expect(
+        useDeleteSearchParam('?topic=api&other-topic=other-api', [
+          'topic',
+          'other-topic',
+        ])
+      ).toBe('');
+    });
+    test('when url has multiple params', () => {
+      expect(
+        useDeleteSearchParam(
+          '?topic=api&other-topic=other-api&and-another=not-api',
+          ['topic', 'other-topic']
+        )
+      ).toBe('and-another=not-api');
+    });
+  });
+});
 
-test('uses current window location search as a default', () => {});
-test('returns error when url is not passed and window is not defined', () => {});
-test('uses search string when provided as an argument', () => {});
+describe('useGetAllSearchParams', () => {
+  test('returns all params with their values', () => {
+    expect(useGetAllSearchParams('?topic=api&other=stuff')).toEqual({
+      topic: 'api',
+      other: 'stuff',
+    });
+  });
+  test('returns all keys of the params', () => {
+    expect(
+      useGetAllSearchParams('?topic=api&other=stuff', { keysOnly: true })
+    ).toEqual(['topic', 'other']);
+  });
+  test('returns all values of the params', () => {
+    expect(
+      useGetAllSearchParams('?topic=api&other=stuff', { valuesOnly: true })
+    ).toEqual(['api', 'stuff']);
+  });
+});
+
+describe('useStringifySearchParams', () => {
+  describe('returns a search string from regular key/value object', () => {
+    test('with single param', () => {
+      expect(useStringifySearchParams({ topic: 'api' })).toBe('topic=api');
+    });
+    test('with multiple params', () => {
+      expect(
+        useStringifySearchParams({ topic: 'api', something: 'else' })
+      ).toBe('topic=api&something=else');
+    });
+  });
+  describe('returns a search string from object that contains array', () => {
+    test('with default config', () => {
+      expect(
+        useStringifySearchParams({ topic: ['api', 'new-api', 'another-api'] })
+      ).toBe('topic=api&topic=new-api&topic=another-api');
+    });
+    describe('with separator config', () => {
+      test.each(['|', ',', ';'])('that equals %s', (separator) => {
+        expect(
+          useStringifySearchParams(
+            { topic: ['api', 'new-api', 'another-api'] },
+            { arrayType: 'separator', separator }
+          )
+        ).toBe(`topic=api${separator}new-api${separator}another-api`);
+      });
+    });
+    test('with bracket config', () => {
+      expect(
+        useStringifySearchParams(
+          { topic: ['api', 'new-api', 'another-api'] },
+          { arrayType: 'bracket' }
+        )
+      ).toBe('topic[]=api&topic[]=new-api&topic[]=another-api');
+    });
+    test('with indexed bracket config', () => {
+      expect(
+        useStringifySearchParams(
+          { topic: ['api', 'new-api', 'another-api'] },
+          { arrayType: 'indexedBracket' }
+        )
+      ).toBe('topic[0]=api&topic[1]=new-api&topic[2]=another-api');
+    });
+  });
+  test('returns a search string from object that contains object', () => {});
+});
+
+// change to nunmber
+// change to boolean
+// decoded encoded
+
+// test('Sorts all key/value pairs by their keys.', () => {});
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,119 @@
-interface Param {
-  key: string;
-  value: string;
+interface GetAllOptions {
+  keysOnly?: boolean;
+  valuesOnly?: boolean;
+}
+
+interface ArrayOptions {
+  arrayType?: 'separator' | 'bracket' | 'indexedBracket';
+  separator?: string;
 }
 
 const useHasSearchParam = (search: string, key: string): boolean =>
   new URLSearchParams(search).has(key);
 
-const useGetSearchParam = (search: string, key: string): string | null =>
-  new URLSearchParams(search).get(key);
-
-const useAppendSearchParam = (
+const useGetSearchParam = (
   search: string,
-  params: Param[] | Param
+  key: string
+): string | string[] | null => {
+  const params = new URLSearchParams(search).getAll(key);
+  return params.length === 0 ? null : params.length === 1 ? params[0] : params;
+};
+
+const useGetAllSearchParams = (
+  search: string,
+  options?: Partial<GetAllOptions>
+): object | string[] => {
+  if (options?.keysOnly) {
+    return Array.from(new URLSearchParams(search).keys());
+  } else if (options?.valuesOnly) {
+    return Array.from(new URLSearchParams(search).values());
+  } else {
+    const entries: string[][] = Array.from(
+      new URLSearchParams(search).entries()
+    );
+    let returnObj = {};
+    entries.forEach((entry: string[]) => {
+      returnObj[entry[0]] = entry[1];
+    });
+    return returnObj;
+  }
+};
+
+const useAppendSearchParam = (search: string, params: object): string => {
+  const startingParams = new URLSearchParams(search);
+  for (let index in params) {
+    startingParams.append(index, params[index]);
+  }
+  return startingParams.toString();
+};
+
+const useSetSearchParam = (search: string, params: object): string => {
+  const startingParams = new URLSearchParams(search);
+  for (let index in params) {
+    startingParams.set(index, params[index]);
+  }
+  return startingParams.toString();
+};
+
+const useDeleteSearchParam = (
+  search: string,
+  keys: string | string[]
 ): string => {
   const startingParams = new URLSearchParams(search);
 
-  if (Array.isArray(params)) {
-    params.forEach((param) => {
-      startingParams.append(param.key, param.value);
+  if (Array.isArray(keys)) {
+    keys.forEach((key) => {
+      startingParams.delete(key);
     });
   } else {
-    startingParams.append(params.key, params.value);
+    startingParams.delete(keys);
   }
 
   return startingParams.toString();
 };
 
-export { useHasSearchParam, useGetSearchParam, useAppendSearchParam };
+const useStringifySearchParams = (
+  params: object,
+  config?: ArrayOptions
+): string => {
+  const startingParams = new URLSearchParams('');
+  for (let index in params) {
+    if (Array.isArray(params[index])) {
+      switch (config?.arrayType) {
+        case 'separator':
+          let mergedValue = params[index]
+            .map((element: string) => element)
+            .join(config?.separator || '|');
+          startingParams.append(index, mergedValue);
+          break;
+        case 'bracket':
+          params[index].forEach((element: string) => {
+            startingParams.append(`${index}[]`, element);
+          });
+          break;
+        case 'indexedBracket':
+          params[index].forEach((element: string, i: number) => {
+            startingParams.append(`${index}[${i}]`, element);
+          });
+          break;
+        default:
+          params[index].forEach((element: string) => {
+            startingParams.append(index, element);
+          });
+      }
+    } else {
+      startingParams.append(index, params[index]);
+    }
+  }
+  return decodeURIComponent(startingParams.toString());
+};
+
+export {
+  useHasSearchParam,
+  useGetSearchParam,
+  useAppendSearchParam,
+  useSetSearchParam,
+  useDeleteSearchParam,
+  useGetAllSearchParams,
+  useStringifySearchParams,
+};
